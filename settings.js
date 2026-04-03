46566e3a455f85f5f41ea263a5ac2d41b80cf4c7
RAHI BOT BBY <rahiahmed143@gmail.com>
2026-04-03 15:01:22 +0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+const settings = {
+  packname: 'Rahi bot',
+  author: 'RAHI BHAII‎',
+  botName: "𝐑𝐀𝐇𝐈 𝐁𝐎𝐓 2°0 ",
+  botOwner: '𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 𝐑𝐀𝐇𝐈', // Your name
+  ownerNumber: '8801751741382', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+};
+
+module.exports = settings;
